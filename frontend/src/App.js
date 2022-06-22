deeab3729f018e7059a5d4065cb7a830eedcfa49
Ma77an <Matang99@gmail.com>
2022-06-22 15:46:49 +0300

    adding products example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 
 function App() {
@@ -7,7 +8,23 @@ function App() {
         <a href="/">Noa BenSa HaZona</a>
       </header>
       <main>
-        פה לשים תמונות של ילדים מתים
+        <h1>Featured Bitches</h1>
+        <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+            <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+              </a>
+              <p><strong>₪{product.price}</strong></p>
+              <button>Add To Cart</button>
+            </div>
+          </div>
+          ))}
+        </div>
       </main>
     </div>
   );
