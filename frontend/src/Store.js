8c52ad3883cf9e5042c7d01c0a10641667618fdc
Ma77an <Matang99@gmail.com>
2022-07-10 14:28:16 +0300

    Complete signin api

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,6 +3,10 @@ import { createContext, useReducer } from 'react';
 export const Store = createContext();
 
 const initalState = {
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null,
+
   cart: {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
@@ -39,6 +43,12 @@ function reducer(state, action) {
         cart: { ...state.cart, cartItems },
       };
     }
+
+    case 'USER_SIGNIN':
+      return { ...state, userInfo: action.payload };
+
+    case 'USER_SIGNOUT':
+      return { ...state, userInfo: null };
 
     default:
       return state;
